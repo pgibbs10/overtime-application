7a66ba407d1ef6dfaaf9b1743d2f4631119c62bd
Paul Gibbs <pwgibbs10@gmail.com>
2018-01-09 14:26:10 -0700

    Implemented the ability to show statuses on the admin dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 
 puts "1 User created"
 
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+
+puts "1 Admin created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
