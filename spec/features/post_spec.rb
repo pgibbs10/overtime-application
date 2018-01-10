b089a1b6b3c8a77ade810d31d77426d53487a898
Paul Gibbs <pwgibbs10@gmail.com>
2018-01-10 13:56:18 -0700

    Implemented tests and page redirection for permission structure

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,15 +11,15 @@ describe 'navigate' do
       visit posts_path
     end
 
-  	it 'can be reached successfully' do
-  		expect(page.status_code).to eq(200)
-  	end
+    it 'can be reached successfully' do
+      expect(page.status_code).to eq(200)
+    end
 
-  	it 'has a title of Posts' do
-  		expect(page).to have_content(/Posts/)
-  	end
+    it 'has a title of Posts' do
+      expect(page).to have_content(/Posts/)
+    end
 
-    it 'has a list of Posts' do
+    it 'has a list of posts' do
       post1 = FactoryGirl.build_stubbed(:post)
       post2 = FactoryGirl.build_stubbed(:second_post)
       visit posts_path
@@ -31,7 +31,7 @@ describe 'navigate' do
     it 'has a link from the homepage' do
       visit root_path
 
-      click_link ("new_post_from_nav")
+      click_link("new_post_from_nav")
       expect(page.status_code).to eq(200)
     end
   end
@@ -41,27 +41,27 @@ describe 'navigate' do
       @post = FactoryGirl.create(:post)
       visit posts_path
 
-      click_link ("delete_post_#{@post.id}_from_index")
+      click_link("delete_post_#{@post.id}_from_index")
       expect(page.status_code).to eq(200)
     end
   end
 
   describe 'creation' do
-  	before do
-  		visit new_post_path
-  	end
+    before do
+      visit new_post_path
+    end
 
-  	it 'has a new form that can be reached' do
-  		expect(page.status_code).to eq(200)
-  	end
+    it 'has a new form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
 
-  	it 'can be created from new form page' do
+    it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
-  	end
+    end
 
     it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
@@ -74,24 +74,29 @@ describe 'navigate' do
 
   describe 'edit' do
     before do
-      @post = FactoryGirl.create(:post)
-    end
-
-    it 'can be described by clicking edit on index page' do
-      visit posts_path
-
-      click_link ("edit_#{@post.id}")
-      expect(page.status_code).to eq(200)
+      @edit_user = User.create(first_name: "asdf", last_name: "asdf", email: "asdfasdf@asdf.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      login_as(@edit_user, :scope => :user)
+      @edit_post = Post.create(date: Date.today, rationale: "asdf", user_id: @edit_user.id)
     end
 
     it 'can be edited' do
-      visit edit_post_path(@post)
+      visit edit_post_path(@edit_post)
 
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Edited content"
       click_on "Save"
 
       expect(page).to have_content("Edited content")
+    end
+
+    it 'cannot be edited by a non authorized user' do
+      logout(:user)
+      non_authorized_user = FactoryGirl.create(:non_authorized_user)
+      login_as(non_authorized_user, :scope => :user)
+
+      visit edit_post_path(@edit_post)
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
