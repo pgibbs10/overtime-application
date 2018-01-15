3e37e5546d73ac45e74352775e9b9cc73cc5ed21
Paul Gibbs <pwgibbs10@gmail.com>
2018-01-15 14:58:06 -0700

    Updated the index and new pages to display overtime hours requested

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,12 @@
 
 puts "1 User created"
 
-AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Test")
 
 puts "1 Admin created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
