fdb42aefd90c794a5399837a06c74108385b4bad
Paul Gibbs <pwgibbs10@gmail.com>
2018-01-25 14:28:44 -0700

    made optional true for belongs to user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 	enum status: { submitted: 0, approved: 1, rejected: 2}
-	belongs_to :user
+	belongs_to :user, optional: true
 	validates_presence_of :date, :rationale, :overtime_request
 
 	validates :overtime_request, numericality: { greater_than: 0.0 }
