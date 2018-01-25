0d603de79d9f09da775efe927f04086b44a40f82
Paul Gibbs <pwgibbs10@gmail.com>
2018-01-25 13:46:39 -0700

    Updated config

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,6 @@ module Overtime
   class Application < Rails::Application
     config.load_defaults 5.1
     config.autoload_paths << Rails.root.join("lib")
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
