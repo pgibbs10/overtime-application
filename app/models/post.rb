4b76e32b324aa3ac08a4b037a4498711813643d0
Paul Gibbs <pwgibbs10@gmail.com>
2018-01-26 10:57:21 -0700

    integrated callback for rejections

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,11 +7,17 @@ class Post < ApplicationRecord
 
 	scope :posts_by, ->(user) { where(user_id: user.id) }
 
-	after_save :update_audit_log
+	after_save :confirm_audit_log, if: :submitted?
+	after_save :un_confirm_audit_log, if: :submitted?
 
 	private
-		def update_audit_log
+		def confirm_audit_log
 			audit_log = AuditLog.where(user_id: self.id, start_date: (self.date - 6.days..self.date)).last
 			audit_log.confirmed! if audit_log
+		end
+
+		def un_confirm_audit_log
+			audit_log = AuditLog.where(user_id: self.id, start_date: (self.date - 6.days..self.date)).last
+			audit_log.pending! if audit_log
 		end
 end
